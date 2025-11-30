6c7c911f99d486f6978524342933ac79a4860109
oscargarcia <ogarjae565@g.educaand.es>
2025-11-30 21:15:37 +0100

    Conversion de json a xml realizada y retoques de nombres en la creacion de archivos

--- src/conversion_xml_a_json/conversor_xml_a_json.py
+++ src/conversion_xml_a_json/conversor_xml_a_json.py
@@ -4,12 +4,12 @@ import xmltodict
 
 def convertir():
     try:
-        with open("src/conversion_xml_a_json/usuarios.xml", "r") as archivo_xml:
+        with open("usuarios.xml", "r") as archivo_xml:
             xml_string = archivo_xml.read()
 
             datos_dict = xmltodict.parse(xml_string)
 
-        with open("src/conversion_xml_a_json/usuarios_ejercicio_externo.json", "w") as archivo: 
+        with open("usuarios.json", "w") as archivo: 
             json.dump(datos_dict, archivo, indent = 4)
 
     except FileNotFoundError:
